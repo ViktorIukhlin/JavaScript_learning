96ead95d1a3141af9a21fda6fd6c59782f43159e
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-07-19 14:34:54 +0300

    ++

--- js/script.js
+++ js/script.js
@@ -10,23 +10,26 @@ const personalMovieDB = {
 };
 
 let question = true;
-let a = []; let b = [];let i = 1;
-while(question == true){
+let a = []; let b = []; let i = 1;
+while (question == true) {
 
-    do{
+    do {
         a[i] = prompt('Один из последних просмотренных фильмов?', '');
     }
-    while((a[i].length == 0)||((a[i].length) > 50));
+    while ((a[i].length == 0) || ((a[i].length) > 50));
 
-    do{
-    b[i] = prompt('На сколько оцените его?', '');
+    do {
+        b[i] = prompt('На сколько оцените его?', '');
     }
-    while((b[i] == 0)||(b[i]> 50));
-    
+    while ((b[i] == 0) || (b[i] > 50));
+
     personalMovieDB.movies[a[i]] = b[i];
 
     question = confirm('Вы хотите проддолжить ввод фильмов?');
     i++;
 }
 
-(personalMovieDB.count < 10)?alert('Просмотрено довольно мало фильмов'):((personalMovieDB.count > 10)&&(personalMovieDB.count < 30))?alert('Вы классический зритель'):alert('Произошла ошибка');
+(personalMovieDB.count < 10) ? alert('Просмотрено довольно мало фильмов')
+: ((personalMovieDB.count >= 10) && (personalMovieDB.count < 30)) ? alert('Вы классический зритель')
+: (personalMovieDB.count >= 30) ? alert('Вы киноман')
+: alert('Произошла ошибка');
